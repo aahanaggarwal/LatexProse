9a56a59698c1e02c6fe5d15f8422d9f0a1ecd021
Prakrit <pduangsu@purdue.edu>
2021-04-11 13:04:40 -0400

    remove System.Tuple

--- part1d-complete/ProseTutorial/synthesis/WitnessFunctions.cs
+++ part1d-complete/ProseTutorial/synthesis/WitnessFunctions.cs
@@ -1,11 +1,10 @@
-﻿using System;
+﻿using System.Linq;
 using System.Collections.Generic;
-using System.Linq;
+using System.Text.RegularExpressions;
 using Microsoft.ProgramSynthesis;
 using Microsoft.ProgramSynthesis.Learning;
 using Microsoft.ProgramSynthesis.Rules;
 using Microsoft.ProgramSynthesis.Specifications;
-using System.Text.RegularExpressions;
 
 namespace ProseTutorial
 {
